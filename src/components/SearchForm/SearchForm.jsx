2baf04bb5d2024a3b506787e82e56562138c672f
t-1000-t <goognya@gmail.com>
2019-11-04 18:22:04 +0200

    fetch

--- src/components/SearchForm/SearchForm.jsx
+++ src/components/SearchForm/SearchForm.jsx
@@ -1,6 +1,5 @@
 import React, { Component, createRef } from 'react';
 // import { ToastContainer, toast, Zoom } from 'react-toastify';
-import shortid from 'shortid';
 import Gallery from '../Gallery/Gallery';
 import styles from './SearchForm.module.css';
 
@@ -11,13 +10,30 @@ class SearchForm extends Component {
 
   static propTypes = {};
 
-  state = {};
+  state = {
+    articles: [],
+  };
+
+  componentDidMount() {
+    fetch(
+      'https://pixabay.com/api/?key=14147963-1172996dfbc4032cf6219bd1a&q=cats&image_type=photo&pretty=true',
+    )
+      .then(res => res.json())
+      .then(data => {
+        this.setState({
+          articles: data.hits,
+        });
+        // console.log(data);
+      });
+  }
 
   render() {
+    const { articles } = this.state;
     return (
       <form className={searchform}>
         <input type="text" autoComplete="off" placeholder="Search images..." />
-        <Gallery />
+        <div>{articles.length > 0 && <Gallery articles={articles} />}</div>
+        {/* <Gallery /> */}
       </form>
     );
   }
